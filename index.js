7f2870095f7c6bb4a8902b273657c0844a935517
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-04-26 14:20:45 -0700

    js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 window.onload = function () {
     let darkmode = localStorage.getItem('darkmode');
     let toggle = document.getElementById('checkbox');
-    if (darkmode) {
+    if (darkmode == 'enabled') {
         document.body.classList.add('darkmode');
         toggle.checked = true;
     }
     toggle.addEventListener('change', function () {
         if (toggle.checked) {
             document.body.classList.add('darkmode');
-            localStorage.setItem('darkmode', true);
+            localStorage.setItem('darkmode', 'enabled');
         } else {
             document.body.classList.remove('darkmode');
-            localStorage.setItem('darkmode', false);
+            localStorage.setItem('darkmode', 'disabled');
         }
     });
 
