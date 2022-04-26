3aca73d4c3e55fcaa175155d0cbeccc3f2b8d934
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-04-26 14:25:25 -0700

    darkmode > ligthmode

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 window.onload = function () {
-    let darkmode = localStorage.getItem('darkmode');
+    let ligthmode = localStorage.getItem('ligthmode');
     let toggle = document.getElementById('checkbox');
-    if (darkmode == 'enabled') {
-        document.body.classList.add('darkmode');
+    if (ligthmode == 'enabled') {
+        document.body.classList.add('ligthmode');
         toggle.checked = true;
     }
     toggle.addEventListener('change', function () {
         if (toggle.checked) {
-            document.body.classList.add('darkmode');
-            localStorage.setItem('darkmode', 'enabled');
+            document.body.classList.add('ligthmode');
+            localStorage.setItem('ligthmode', 'enabled');
         } else {
-            document.body.classList.remove('darkmode');
-            localStorage.setItem('darkmode', 'disabled');
+            document.body.classList.remove('ligthmode');
+            localStorage.setItem('ligthmode', 'disabled');
         }
     });
 
